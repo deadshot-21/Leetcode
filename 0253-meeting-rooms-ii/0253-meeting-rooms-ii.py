8756a0bb7e441373749c4c824542d1f9cb93006e
Soham Faldu <85991495+deadshot-21@users.noreply.github.com>
2025-06-17 18:41:08 -0400

    Time: 7 ms (33.5%), Space: 20.1 MB (26.61%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -3,18 +3,19 @@ class Solution:
         
         if not intervals:
             return 0
-        
+
         start = sorted([i[0] for i in intervals])
         end = sorted([i[1] for i in intervals])
         count, res = 0, 0
-        s,e = 0, 0
-        while s < len(intervals):
+        s,e = 0,0
+        while s<len(intervals):
             if start[s] < end[e]:
                 s+=1
-                count += 1
+                count+=1
             else:
-                e += 1
-                count -= 1
+                e+=1
+                count-=1
             res = max(res,count)
         return res
+        
             
