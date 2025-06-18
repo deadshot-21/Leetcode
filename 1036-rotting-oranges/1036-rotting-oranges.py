9e35bad0ff9e58f4f2dd7e256007c0149aeb8bc7
Soham Faldu <85991495+deadshot-21@users.noreply.github.com>
2025-06-18 10:14:39 -0400

    Time: 2 ms (86.78%), Space: 17.7 MB (71.22%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,28 +1,26 @@
-from collections import deque
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        queue = deque()
+        r,c = len(grid), len(grid[0])
+
         fresh = 0
-        
-        for i in range(m):
-            for j in range(n):
+        queue = deque()
+
+        for i in range(r):
+            for j in range(c):
                 if grid[i][j] == 2:
-                    queue.append((i, j, 0))
+                    queue.append((i,j,0))
                 if grid[i][j] == 1:
-                    fresh += 1
+                    fresh+=1
         
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        minutes = 0
-        
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        minutes=0
         while queue:
-            i, j, minutes = queue.popleft()
-            for di, dj in directions:
-                ni, nj = i + di, j + dj
-                if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == 1:
-                    grid[ni][nj] = 2
+            i,j, minutes = queue.popleft()
+            for ni, nj in directions:
+                if 0<=i+ni<r and 0<=j+nj<c and grid[i+ni][j+nj] == 1:
+                    grid[i+ni][j+nj] = 2
                     fresh -= 1
-                    queue.append((ni, nj, minutes + 1))
+                    queue.append((i+ni,j+nj,minutes+1))
         
-        return minutes if fresh == 0 else -1
+        return minutes if not fresh else -1
+            
